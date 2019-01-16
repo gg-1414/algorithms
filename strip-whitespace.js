2d501b70ef48c195e899d035b926a8eec8575b3e
github username <github email address>
2019-01-15 20:25:34 -0500

    strip whitespace algorithm challenge

--- strip-whitespace.js
+++ strip-whitespace.js
@@ -1,6 +1,22 @@
 function strip(str) {
   //"       adopt a dog!\n\n\n"
   //get rid of extra spaces in the beginning
-  //get rid of extra spaces in the middle if any
+  let indexZero = 0
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] !== " ") {
+      indexZero = i;
+      break;
+    }
+  }
+  let returnString = str.slice(indexZero)
+  return returnString
   //get rid of extra spaces in the end
+  // for(let i = returnString.length - 1; i > 0; i--) {
+  //   if(returnString[i] !== "\n") {
+  //     indexZero = i;
+  //   }
+  // }
+  // return returnString.slice(0, indexZero)
 }
+
+console.log(strip("       adopt a dog!\n\n\n"))
