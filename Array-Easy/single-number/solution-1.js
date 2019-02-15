45ee35878844093e001ebf83982358dab3fd7db6
github username <github email address>
2019-02-15 14:55:10 -0500

    new algorithm

---
+++ Array-Easy/single-number/solution-1.js
@@ -0,0 +1,11 @@
+var singleNumber = function(nums) {
+    var dict = {}; // start with empty object to be used as a dictionary to find the only single element
+    nums.forEach(num => {
+        if (dict[num] !== undefined) { // if dict has a key with num that exists
+            dict[num] = true; // set to true
+        } else { // if dict does not already have a key with the value of num
+            dict[num] = false; // create a new key-value where key is set to num and value is false
+        }
+    })
+    return Object.keys(dict).find(key => dict[key] === false) // find the only false value and return is key
+};
