63de7813a4a7dd8917946c97ad6a7b64de6f8a76
github username <ginalee1114@gmail.com>
2019-03-05 23:58:06 -0500

    new solution for remove duplicates algorithm challenge

---
+++ Array-Easy/remove-duplicates-from-sorted-array/solution-2.js
@@ -0,0 +1,11 @@
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) { return 0 }
+    let count = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[count]) {
+            count++;
+            nums[count] = nums[i];
+        }
+    }
+    return count + 1;
+}
